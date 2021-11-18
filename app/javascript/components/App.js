2315d97436943f36050c1824c7315593cbec6144
alappie <ashley@specialtyproduce.com>
2021-11-18 13:06:20 -0800

    no changes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -21,6 +21,12 @@ class App extends React.Component {
     super(props);
   }
   render () {
+    console.log("logged in:", this.props.logged_in);
+    console.log("current user:", this.props.current_user);
+    console.log("sign up", this.props.new_user_route);
+    console.log("sign in:", this.props.sign_in_route);
+    console.log("sign out:", this.props.sign_out_route);
+
     return (
       <>
         <Router>
