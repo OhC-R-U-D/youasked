4d94c3df698cfa5dc6e42fbe3c26948b555b1164
LiangchengR <liangchengr@gmail.com>
2021-11-18 16:33:01 -0800

    added some team information and cards for each team member

--- app/javascript/pages/AboutUsPage.js
+++ app/javascript/pages/AboutUsPage.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-export default class AboutUsPage extends Component {
+export default class AboutUsPage extends Component {  
     render() {
+         const team = [
+           { name: "Ashley Lappies", img: "", blurb: "" },
+           { name: "Kevin Silver", img: "", blurb: "" },
+           { name: "Rebecca Reynolds", img: "", blurb: "" },
+           { name: "Shaun Sharp", img: "", blurb: "" },
+         ];
         return (
-            <div>
-                This is the About Us Page
-            </div>
-        )
+          <div className="about-us-page">
+            <h1>Meet the Team</h1>
+
+            {team.map(person => {
+                return (
+                  <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                      <Typography variant="h5" component="div">
+                        Name
+                      </Typography>
+
+                      <Typography variant="body2">lorem ipsum</Typography>
+                    </CardContent>
+                  </Card>
+                );})} 
+          </div>
+        );
     }
 }
 
