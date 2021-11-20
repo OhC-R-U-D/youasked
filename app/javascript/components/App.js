13955efb4c17ac85c85ab4775783d6a3d7c66d2f
LiangchengR <liangchengr@gmail.com>
2021-11-20 13:01:49 -0800

    added question rcreate/POST and delete/DESTROY requests, adjusted routes to pass in correct props

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
@@ -15,21 +13,54 @@ import QuestionNew from "../pages/QuestionNew";
 import QuestionShow from "../pages/QuestionShow";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state={
-      questions:[],
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+    };
   }
+
   componentDidMount() {
     this.questionRead();
   }
-  questionRead=()=> {
+
+  questionRead = () => {
     fetch("/questions")
-    .then(response => response.json())
-    .then(payload => this.setState({ questions: payload }))
-    .catch(errors => console.log("Questions Index Errors:", errors))
-  }
+      .then((response) => response.json())
+      .then((payload) => this.setState({ questions: payload }))
+      .catch((errors) => console.log("Questions Index Errors:", errors));
+  };
+
+  createNewQuestion = () => {
+    fetch("/questions", {
+      body: JSON.stringify(newQuestion),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    })
+      .then((response) => {
+        if (response.status === 422) {
+          alert("Something went wrong, please check your submission.");
+        }
+        return response.json();
+      })
+      .then((payload) => this.questionRead())
+      .catch((errors) => console.log("Question create errors:", errors));
+  };
+
+  deleteQuestion = (id) => {
+    fetch(`/questions/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((payload) => this.questionRead())
+      .catch((errors) => console.log("Question delete errors:", errors));
+  };
+
   render() {
     return (
       <BrowserRouter>
@@ -40,23 +71,46 @@ class App extends React.Component {
           <Route path="/error" component={Page404} />
           <Route path="/guidelines" component={CommunityGuidelinesPage} />
           <Route path="/contact" component={Contact} />
-          <Route path="/protectedindex" component={ProtectedIndex} />
-          <Route 
+          <Route
+            path="/protectedindex"
+            render={() => {
+              let myQuestions = this.state.questions.filter(
+                (question) => question.user_id === this.props.current_user.id
+              );
+              //to do: need to also filter out current users answers
+              return (
+                <ProtectedIndex
+                  questions={myQuestions}
+                  deleteQuestion={this.deleteQuestion}
+                />
+              );
+              //to do: need to also pass in answers and deleteAnswer as props
+            }}
+          />
+          <Route
             path="/questionindex"
-            render={props => <QuestionIndex user={this.props.current_user} 
-            questions={this.state.questions} />} 
+            render={() => (
+              <QuestionIndex
+                user={this.props.current_user}
+                questions={this.state.questions}
+              />
+            )}
           />
           <Route path="/questionnew" component={QuestionNew} />
-          <Route 
-          path="/questionshow/:id"
-          render={props => {
-              let id = props.match.params.id
-              let question = this.state.questions.find((c) => c.id === +id)
-              console.log(question)
-              return <QuestionShow question={question} />
-              }
-              }
-               />
+          <Route
+            path="/questionshow/:id"
+            render={(props) => {
+              let id = props.match.params.id;
+              let question = this.state.questions.find((c) => c.id === +id);
+              //to do: need to filter answers based on question id
+              return (
+                <QuestionShow
+                  question={question}
+                  //to do: pass in answers and editAnswers as props 
+                />
+              );
+            }}
+          />
         </Switch>
         <Footer />
       </BrowserRouter>
