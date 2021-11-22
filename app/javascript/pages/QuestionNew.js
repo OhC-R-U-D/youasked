6c2aff33b88f151f0e6143075f85682a814e16a4
alappie <ashley@specialtyproduce.com>
2021-11-21 23:17:15 -0800

    Functionality written, no endpoints to test.

--- app/javascript/pages/QuestionNew.js
+++ app/javascript/pages/QuestionNew.js
@@ -4,7 +4,28 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import formpic from "../assets/questionform.jpeg";
 
+
+
 export default class QuestionNew extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            form: {
+             question: "",
+             url: ""   
+            },
+            submitted: false
+        }
+    }
+    handleChange = (e) => {
+        let { form } = this.state
+        form[e.target.name] = e.target.value
+        this.setState({form: form})
+    }
+    handleSubmit = () => {
+        this.props.createNewQuestion(this.state.form)
+        this.setState({submitted: true})
+      }
     render() {
         return (
             <Box id="form"
@@ -12,11 +33,16 @@ export default class QuestionNew extends Component {
             sx={{ '&.MuiTextFeild-root': { m: 1, width: '25ch' },}}
             noValidateautoComplete="off">
                <div>
+               <img src={formpic} alt="formpic" className="formpic"/>
+               <br />
+               <br />
                <TextField 
                         required
                         id="outlined-required"
                         label="Optional"
                         defaultValue="URL"
+                        onChange={this.handleChange}
+                        value={this.state.form.url} 
                         />
                         <br />
                     <TextField fullWidth sx={{ m: 1 }}
@@ -24,9 +50,14 @@ export default class QuestionNew extends Component {
                         id="outlined-required"
                         label="Required"
                         defaultValue="SPILL YOUR GUTS HERE"
+                        onChange={this.handleChange}
+                        value={this.state.form.question}
                         />
                          <br />
-                        <Button size="medium">Submit</Button>
+                        <Button 
+                        size="medium" 
+                        onClick={this.handleSubmit}>Submit</Button>
+                        {this.state.submitted && <Redirect to="/protectedindex" />}
                    </div> 
             </Box>
 
