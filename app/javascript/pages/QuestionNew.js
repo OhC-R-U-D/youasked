90aecb77edea8dd4ee7dfc114f2bc553c5d97a65
alappie <ashley@specialtyproduce.com>
2021-11-21 22:46:27 -0800

    form box in place, no functionality

--- app/javascript/pages/QuestionNew.js
+++ app/javascript/pages/QuestionNew.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import formpic from "../assets/questionform.jpeg";
 
 export default class QuestionNew extends Component {
     render() {
         return (
-            <div>
-                This is the Question New Index
-            </div>
+            <Box id="form"
+            component="form"
+            sx={{ '&.MuiTextFeild-root': { m: 1, width: '25ch' },}}
+            noValidateautoComplete="off">
+               <div>
+               <TextField 
+                        required
+                        id="outlined-required"
+                        label="Optional"
+                        defaultValue="URL"
+                        />
+                        <br />
+                    <TextField fullWidth sx={{ m: 1 }}
+                        required
+                        id="outlined-required"
+                        label="Required"
+                        defaultValue="SPILL YOUR GUTS HERE"
+                        />
+                         <br />
+                        <Button size="medium">Submit</Button>
+                   </div> 
+            </Box>
+
         )
     }
 }
