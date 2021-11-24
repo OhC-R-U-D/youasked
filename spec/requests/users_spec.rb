f144b2eac44972469b80a8bf518575287b9fa757
silverkd70 <silverkd70@icloud.com>
2021-11-23 21:37:55 -0800

    added users controller, endpoint and setup app.js fetch for users

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+
+  # describe "get /index" do
+  #   it "gets a list of users" do
+  #     User.create alias: 'testing_alias', email: 'testing@testing.com', password: '123456', password_confirmation: "123456"
+
+  #     get '/users'
+
+  #     user = JSON.parse(response.body)
+  #     expect(response).to have_http_status(200)
+  #     expect(cat.length).to eq 1
+  #   end
+  # end
+
+end
