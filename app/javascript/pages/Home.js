d98b0dda36de55b7f198836e3b8e49223e753f0a
alappie <ashley@specialtyproduce.com>
2021-11-25 21:40:56 -0800

    added logo, made landing page responsive

--- app/javascript/pages/Home.js
+++ app/javascript/pages/Home.js
@@ -2,33 +2,37 @@ import React, { Component } from 'react'
 import landingpage from '../assets/landingpage.jpg'
 import landingmedia from '../assets/landingpage.gif'
 import Box from '@mui/material/Box';
+import logo from '../assets/logo.png'
 
 
 export default class Home extends Component {
     render() {
         return (
-            <div id="body-container">
+            <div class="landing-container">
+                {/* <div>
+                    <h1 id="landing">YOU ASKED..</h1>
+                </div> */}
                 <div>
-                <h1 id="landing">YOU ASKED..</h1>
-                </div>
-                <div>
-                <img src={landingmedia} alt="landingmedia" className="landingmedia"/>
-                </div>
-                <div>
-                <img src={landingpage} alt="landingpage" className="landingpage"/>
+                    <img src={landingmedia} alt="landingmedia" className="landingmedia"/>
                 </div>
                 <div>
-                <Box class="landing-quote" sx={{ display: 'flex', justifyContent: 'center', borderRadius: 16 }}>
-                <p>Sometimes life isn’t black and white 
-                    and we’re faced with questions,	
-                    but let’s keep it light!
-                Post your questions if you have one now…	
-                but responses may be below the brow.</p>
-                </Box>
+                    <img src={logo}  
+                    alt="logo"
+                    className="logo"/>
                 </div>
+                <div>
+                <   img src={landingpage} alt="landingpage" className="landingpage"/>
                 </div>
-            
-
+                <div>
+                    <Box class="landing-quote" sx={{ display: 'flex', justifyContent: 'center', borderRadius: 16 }}>
+                    <p>Sometimes life isn’t black and white 
+                        and we’re faced with questions,	
+                        but let’s keep it light!
+                    Post your questions if you have one now…	
+                    but responses may be below the brow.</p>
+                    </Box>
+                </div>
+            </div>
         )
     }
 }
