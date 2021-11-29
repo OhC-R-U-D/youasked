bf69089b6743d2a6d37decb0387545e39c6c2f5e
alappie <ashley@specialtyproduce.com>
2021-11-29 12:41:34 -0800

    Format on Questions new

--- app/javascript/pages/QuestionNew.js
+++ app/javascript/pages/QuestionNew.js
@@ -1,71 +1,73 @@
-import React, { Component } from 'react'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import React, { Component } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import formpic from "../assets/questionform.jpeg";
 import { Redirect } from "react-router-dom";
 
-
-
-
-
 export default class QuestionNew extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            form: {
-             post: "",
-             img_url: "",
-             user_id: this.props.current_user.id
-            },
-            submitted: false
-        }
-    }
-    handleChange = (name, e) => {
-        let { form } = this.state
-        form[name] = e.target.value
-        this.setState({form: form})
-    }
-    handleSubmit = () => {
-        this.props.createNewQuestion(this.state.form)
-        this.setState({submitted: true})
-      }
-    render() {
-        return (
-            <Box id="form"
-            component="form"
-            sx={{ '&.MuiTextFeild-root': { m: 1, width: '25ch' },}}
-            noValidateautoComplete="off">
-               <div>
-               <img src={formpic} alt="formpic" className="formpic"/>
-               <br />
-               <br />
-               <TextField 
-                        id="outlined-required"
-                        label="URL"
-                        onChange={(e)=>{
-                            console.log(e)
-                            this.handleChange("img_url", e)}}
-                        value={this.state.form.img_url} 
-                        />
-                        <br />
-                    <TextField fullWidth sx={{ m: 1 }}
-                        id="question-box"
-                        label="SPILL YOUR GUTS HERE"
-                        onChange={(e)=>this.handleChange("post", e)}
-                        value={this.state.form.post}
-                        />
-                         <br />
-                        <Button 
-                        size="medium" 
-                        variant="contained"
-                        color="secondary"
-                        onClick={this.handleSubmit}>Submit</Button>
-                        {this.state.submitted && <Redirect to="/questionindex" />}
-                   </div> 
-            </Box>
-
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: {
+        post: "",
+        img_url: "",
+        user_id: this.props.current_user.id,
+      },
+      submitted: false,
+    };
+  }
+  handleChange = (name, e) => {
+    let { form } = this.state;
+    form[name] = e.target.value;
+    this.setState({ form: form });
+  };
+  handleSubmit = () => {
+    this.props.createNewQuestion(this.state.form);
+    this.setState({ submitted: true });
+  };
+  render() {
+    return (
+      <Box
+        id="form"
+        component="form"
+        sx={{ "&.MuiTextFeild-root": { m: 1, width: "25ch" } }}
+        noValidateautoComplete="off"
+      >
+        <div>
+          <img src={formpic} alt="formpic" className="formpic" />
+          <br />
+          <br />
+          <TextField
+            id="outlined-required"
+            label="URL"
+            onChange={(e) => {
+              console.log(e);
+              this.handleChange("img_url", e);
+            }}
+            value={this.state.form.img_url}
+          />
+          <br />
+          <TextField
+            fullWidth
+            sx={{ m: 1 }}
+            id="question-box"
+            label="SPILL YOUR GUTS HERE"
+            onChange={(e) => this.handleChange("post", e)}
+            value={this.state.form.post}
+          />
+          <br />
+          <Button
+            size="medium"
+            variant="contained"
+            color="secondary"
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </Button>
+          {this.state.submitted && <Redirect to="/questionindex" />}
+        </div>
+      </Box>
+    );
+  }
 }
-
