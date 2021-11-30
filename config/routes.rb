2ace3472e9fad4555aaa40692e72acb1c62a7084
silverkd70 <silverkd70@icloud.com>
2021-11-30 09:59:54 -0800

    setup redirects for user sign up and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   resources :answers
   resources :questions
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations"}
   get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
   root 'home#index'
 end
